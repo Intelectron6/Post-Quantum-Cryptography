328d5f9311b221c24eae2d4d8c06bc7912ace5ee
Dhruva Hegde <53150130+Intelectron6@users.noreply.github.com>
2023-07-12 00:13:01 +0530

    Update module_lwe_v2.py

--- Module-LWE-v2/module_lwe_v2.py
+++ Module-LWE-v2/module_lwe_v2.py
@@ -47,27 +47,45 @@ def poly_mul_ntt(x1, x2):
 
     return z
 
+
 # Key generation function (to be performed by server)
 def key_gen():
-    a = np.random.randint(q, size=(n2,))
-    s = np.random.randint(-5,5, size=(n2,))
-    e = np.random.randint(-5,5, size=(n2,))
-    b = (poly_mul_ntt(a, s) + e) % q
-    return s, a, b
+	a = np.random.randint(q, size=(k,k,n2))
+	s = np.random.randint(-2, 2, size=(k,n2))
+	e = np.random.randint(-2, 2, size=(k,n2))
+	b0 = (poly_mul_ntt(a[0,0], s[0]) + e[0]) % q
+	b1 = (poly_mul_ntt(a[0,1], s[1]) + e[1]) % q
+	b2 = (poly_mul_ntt(a[1,0], s[0]) + e[0]) % q
+	b3 = (poly_mul_ntt(a[1,1], s[1]) + e[1]) % q
+	b01 = (b0 + b1) % q
+	b23 = (b2 + b3) % q
+	b = np.array([b01, b23])
+	return s, a, b
 
 # Encryption function (to be performed by client)
 def encrypt(a, b, m):
-    r = np.random.randint(-5,5, size=(n2,))
-    e1 = np.random.randint(-5,5, size=(n2,))
-    e2 = np.random.randint(-5,5, size=(n2,))
-    u = (poly_mul_ntt(a, r) + e1) % q
-    v = (poly_mul_ntt(b, r) + e2 + math.floor(q/2) * m) % q
-    return u, v
+	r = np.random.randint(-2, 2, size=(k,n2))
+	e1 = np.random.randint(-2, 2, size=(k,n2))
+	e2 = np.random.randint(-2, 2, size=(n2,))
+	u0 = (poly_mul_ntt(a[0,0], r[0]) + e1[0]) % q
+	u1 = (poly_mul_ntt(a[1,0], r[1]) + e1[1]) % q
+	u2 = (poly_mul_ntt(a[0,1], r[0]) + e1[0]) % q
+	u3 = (poly_mul_ntt(a[1,1], r[1]) + e1[1]) % q
+	u01 = (u0 + u1) % q
+	u23 = (u2 + u3) % q
+	u = np.array([u01, u23])
+	v0 = np.array(poly_mul_ntt(b[0], r[0]))
+	v1 = np.array(poly_mul_ntt(b[1], r[1]))
+	v = (v0 + v1 + e2 + math.floor(q/2)*m) % q
+	return u, v
 
 # Decryption function (to be performed by server)
 def decrypt(s, u, v):
-    d = (v - poly_mul_ntt(s, u)) % q
-    return d
+        p0 = np.array(poly_mul_ntt(s[0], u[0]))
+        p1 = np.array(poly_mul_ntt(s[1], u[1]))
+        p = (p0 + p1) % q
+        d = (v - p) % q
+        return d
 
 
 # Randomly generated binary message, m
